cbffb9180402e782cb42bba73972ed163e00036f
MdialloC19 <mdiallo3211@gmail.com>
2023-03-09 13:04:05 +0000

    utilisation de l'argumenent

--- MyFirst.java
+++ MyFirst.java
@@ -1,7 +1,9 @@
 import java.lang.*;
-class MyFirst
+public class MyFirst
 {
     public static void main(String arg[]){
         System.out.println("Hello World");
+        System.out.println(arg[0]);
+        System.out.println(arg[1]);
     }
 }
