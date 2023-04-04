e4fb6a6c939c77473a603b9bea8cb911a5876088
MdialloC19 <mdiallo3211@gmail.com>
2023-04-04 20:20:25 +0000

    Betewise in deepdive

--- Bitewise.java
+++ Bitewise.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.lang.*;
 public class Bitewise {
     public static void main(String [] args){
         int x=10,y=6,z,w;
@@ -6,8 +8,24 @@ public class Bitewise {
         w=~e;
         //z=x^y;
         System.out.println(String.format("%s",Integer.toBinaryString(z)));
-        System.out.println(String.format("%s",Integer.toBinaryString(e)));
+        System.out.println(String.format("%32s",Integer.toBinaryString(e)));
         System.out.println(String.format("%s",Integer.toBinaryString(w)));
+        System.out.println(w);
+        //Swapping using bitewise xor
+        int a =10, b=15;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("a="+a+" b= "+b);
+        //Masking and Mergin
+        byte c=9, d=12, f=0;
+        f=(byte)(c<<4);
+        f=(byte)(f|d);
+        System.out.println((f&0b11110000)>>4);
+        System.out.println((f&0b0001111));
+       // System.out.println(String.format("%8s",Integer.toBinaryString(f)));
+
+
     }
     
 }
