2630e6af74b05897b33f75d8241513a700af3d02
MdialloC19 <mdiallo3211@gmail.com>
2023-04-07 14:06:13 +0000

    Formatted output 2

--- Formatted.java
+++ Formatted.java
@@ -1,11 +1,17 @@
 import java.lang.*;
 public class Formatted {
     public static void main (String []args){
-        int x=10;
+        //index, conversion for formatting output
+       /* int x=10;
         float y=0.0012f;
         char z='A';
         String str="Java Programming ";
-        System.out.printf("%3$s %2$e %1$d \n ",x,y,str);
+        System.out.printf("%3$s %2$e %1$d \n ",x,y,str);*/
+        //flag and width
+        int a=10;
+        int b=-10;
+        System.out.printf("%05d\n ",a);
+        System.out.printf("%+5d\n ",b);// the minus is replaced by () when the number is negatif.
     }
     
 }
