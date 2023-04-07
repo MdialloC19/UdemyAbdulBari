5dc68b67df9f52eff5ba4b11bb2a493362e5dfb5
MdialloC19 <mdiallo3211@gmail.com>
2023-04-07 21:01:48 +0000

    String

---
+++ StringPractice.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+public class StringPractice {
+    public static void main(String []args){
+        /*String str1= "Java Programm";
+        String str2=new String("JAVA");
+        char c[]={'H','e','l','l','o'};
+        byte b[]={65,66,67,68};
+        String str3=new String(c);
+        String str4=new String(b,2,2);
+        System.out.println(str4);*/
+        String str1= "Java";
+        String str2= new String("JAVA");
+        System.out.println(str1==str2);
+    }
+    
+}
