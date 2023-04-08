4f40e54a5f369cd7a399ff77398feaa3e8e77ee2
MdialloC19 <mdiallo3211@gmail.com>
2023-04-08 08:46:47 +0000

    String méthoids1

---
+++ StringMethods.java
@@ -0,0 +1,11 @@
+public class StringMethods {
+    public static void main(String [] args){
+        String str=new String("abdulBari");
+        //str=str.toUpperCase();
+        str=str.trim();
+        String str1=str.substring(3,8);
+        String str3=str.replace('a','e');
+        System.out.println(str3);
+    }
+    
+}
