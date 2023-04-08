ab604c44a15db5b91d461dc1eabc8eff039588fc
MdialloC19 <mdiallo3211@gmail.com>
2023-04-08 22:03:13 +0000

    String Method last

--- StringMethods.java
+++ StringMethods.java
@@ -16,6 +16,16 @@ public class StringMethods {
         String strn1="www.udemy.co.in";
         System.out.println(strn1.indexOf('.', 4));
         System.out.println(strn1.lastIndexOf('.'));
+        //Methods 3
+        String strn2="pyramid wall";
+        String strn3="Pyramid";
+        String strn4=new String("Pyramid");//create a new object in the heap, witch have different reference.
+        System.out.println(strn2.equalsIgnoreCase(strn3));
+        System.out.println(strn2==(strn3));
+        System.out.println("resulat "+strn2.compareTo(strn3));// if they aren't equal they give the difference between their ascii code
+        System.out.println(strn2.contains("wall"));
+
+
 ;    }
     
 
