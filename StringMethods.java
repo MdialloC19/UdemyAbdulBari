4c24356dce266aaf0f27b61264e57c08ac6f3f5d
MdialloC19 <mdiallo3211@gmail.com>
2023-04-08 21:37:24 +0000

    String Method 2

--- StringMethods.java
+++ StringMethods.java
@@ -6,6 +6,17 @@ public class StringMethods {
         String str1=str.substring(3,8);
         String str3=str.replace('a','e');
         System.out.println(str3);
-    }
+        //Methods 2
+        String strn="Mr. Shahruh Khan";
+        System.out.println(strn.startsWith("Shah",4));
+        System.out.println(strn.endsWith("Khan"));
+        for(int i=0; i<strn.length();i++)
+            System.out.print(strn.charAt(i));
+        
+        String strn1="www.udemy.co.in";
+        System.out.println(strn1.indexOf('.', 4));
+        System.out.println(strn1.lastIndexOf('.'));
+;    }
     
+
 }
