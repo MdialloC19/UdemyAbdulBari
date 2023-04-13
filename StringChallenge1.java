b45d6b39a18a0a1a5fcd8ab8bd8ce133a44507d4
MdialloC19 <mdiallo3211@gmail.com>
2023-04-13 23:55:05 +0000

    Last for pattern

---
+++ StringChallenge1.java
@@ -0,0 +1,9 @@
+public class StringChallenge1{
+    public static void main(String []args){
+        int number=010101;
+        String str="010101";
+        System.out.println(str.matches("[O1]+"));//Find if the email id is on gmail with regex
+
+       
+    }
+}
