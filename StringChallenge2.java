a5282b65b1116080ac44bbf5fd8f5e3c43f499f2
MdialloC19 <mdiallo3211@gmail.com>
2023-04-14 02:50:52 +0000

    String chalenge 2 with regex

---
+++ StringChallenge2.java
@@ -0,0 +1,15 @@
+public class StringChallenge2 {
+    public static void main(String []args){
+
+    
+    // Remove Special characters from string
+    String str="a!B@C#1$2%3";
+    System.out.println(str.replaceAll("[^a-zA-Z0-9]",""));
+    // Remove extra spaces from string
+    String str1="   abc     abc  de  fgh ijk ";
+    str1=str1.replaceAll("[\\s]+"," ").trim();
+    String words[]=str1.split("\\s");
+    System.out.println(words.length);
+
+    }
+}
