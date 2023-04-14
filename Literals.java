0b973ec537ab95c187a2022f5af8cb9e7fdc4594
MdialloC19 <mdiallo3211@gmail.com>
2023-04-14 02:23:00 +0000

    FInalisation of chalenge #1

--- Literals.java
+++ Literals.java
@@ -1,7 +1,7 @@
 
 public class Literals {
     public static void main(String[] args) {
-        /*byte b1=10;
+        byte b1=10;
         byte b2=0b1010;
         byte b3=012;
         byte b4=0XA;
@@ -9,7 +9,7 @@ public class Literals {
         System.out.println(b1);
         System.out.println(b2);
         System.out.println(b3);
-        System.out.println(b4);*/
+        System.out.println(b4);
         
         long l=9999999999L;
         float f=12.56f;
--- StringChallenge1.java
+++ StringChallenge1.java
@@ -1,8 +1,19 @@
 public class StringChallenge1{
     public static void main(String []args){
-        int number=010101;
-        String str="010101";
-        System.out.println(str.matches("[O1]+"));//Find if the email id is on gmail with regex
+        System.out.println("Cheking if a string is Binary number");
+        int b=0b011101;
+        String str= Integer.toBinaryString(b);
+        System.out.println(str);
+        System.out.println(str.matches("[01]*"));
+
+        System.out.println("Cheking if a string is hexadecimal number");
+        String str1 ="234AB";
+        System.out.println(str1.matches("[0-9A-Z]+"));
+
+        System.out.println("Cheking if a data string is data");
+        String data="01/02/2023";
+        System.out.println(data.matches("[0-3]/[0-1][0-9]/[0-9]{4}"));
+
 
        
     }
