134a0e2e634b8f7a70465a48d2f9bdd35bd7b50b
MdialloC19 <mdiallo3211@gmail.com>
2023-04-20 01:35:17 +0000

    Challenge 1 pattern

---
+++ ChalengePatterns1.java
@@ -0,0 +1,31 @@
+public class ChalengePatterns1 {
+    public static void main(String [] args){
+        System.out.println("Chalenge Pattern1");
+
+        for( int i=1;i<=5;i++){
+            for(int j=1; j<=5; j++){
+                System.out.print(j+ " "); 
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Chalenge Pattern2");
+
+        for( int i=1;i<=5;i++){
+            for(int j=1; j<=5; j++){
+                System.out.print(i+ " "); 
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Chalenge Pattern 3");
+
+        for( int i=1;i<=5;i++){
+            for(int j=1; j<=5; j++){
+                System.out.print(i+j+ " "); 
+            }
+            System.out.println("");
+        }
+    }
+    
+}
