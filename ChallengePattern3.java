4cfc8b9011d12c7d66d0447df378129aa6668352
MdialloC19 <mdiallo3211@gmail.com>
2023-04-20 23:39:55 +0000

    Big chalenge

--- ChallengePattern3.java
+++ ChallengePattern3.java
@@ -25,6 +25,25 @@ public class ChallengePattern3 {
             } 
             System.out.println("");
         }
+        System.out.println("Challenge Pattern 3");
+        int i;
+        for(i=1;i<=5;i++){
+            int j=0;
+            for(j=1; j<=5; j++){
+                if(i+j>5)
+                    System.out.print("*");
+                else                    
+                    System.out.printf(" "); 
+            }
+            for(int k=j+1; k<i+j; k++){
+                if(i+j>5)
+                    System.out.print("*");
+                else                    
+                    System.out.printf(" "); 
+
+            } 
+            System.out.println("");
+        }
     }
 }
 
