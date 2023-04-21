101406657ee7db0964eedc249b3cbd0c69be9c57
MdialloC19 <mdiallo3211@gmail.com>
2023-04-21 00:17:12 +0000

    Sum of element of array

---
+++ ArrayPractice.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ArrayPractice {
+    public static void main(String [] args){
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int Sum=0;
+        Scanner src=new Scanner(System.in);
+        System.out.println("Finding Sum of all elements!!!");
+        for(int i=0; i<A.length;i++){
+            Sum+=A[i];
+        }
+        System.out.println(" Sum = "+Sum);
+
+        System.out.println("Searching an element!!! \nVoici les élements du tableau");
+        System.out.print(" A[]= ");
+        for(int i=0; i<A.length;i++){
+            System.out.print(A[i]+ " ");
+        }
+        System.out.println(" Veuillez saisr l'éléments à rechercher");
+        int elt=src.nextInt();
+        
+        
+    }
+}
