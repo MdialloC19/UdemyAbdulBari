c287c7e07d844378549d6c00640c944fac62673e
MdialloC19 <mdiallo3211@gmail.com>
2023-04-21 00:35:30 +0000

    Searching an element of tab

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ArrayPractice {
     public static void main(String [] args){
         int A[]={3,9,7,8,12,6,15,5,4,10};
-        int Sum=0;
+        int Sum=0,find=0;
         Scanner src=new Scanner(System.in);
         System.out.println("Finding Sum of all elements!!!");
         for(int i=0; i<A.length;i++){
@@ -18,6 +18,18 @@ public class ArrayPractice {
         }
         System.out.println(" Veuillez saisr l'éléments à rechercher");
         int elt=src.nextInt();
+        for(int i=0; i<A.length;i++){
+            if(elt==A[i]){
+                find=i;
+            }
+
+        }
+        if(find!=0)
+            System.out.println("L'élément se trouve à l'index: "+find);
+        else
+            System.out.println("L'élément se trouve pas dans le tableau");
+            
+        
         
         
     }
