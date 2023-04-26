9930afe3a617286342c930a27207b4a53bf5739f
MdialloC19 <mdiallo3211@gmail.com>
2023-04-26 00:34:40 +0000

    Reverse array

--- ArrayPractice2.java
+++ ArrayPractice2.java
@@ -6,12 +6,22 @@ public class ArrayPractice2 {
         int T[]={8,5,12,34,10,11,2,4,5,9,0};
         int L=T.length;
         int B[]=new  int [L];
+        int C[]=new int[L];
         
         System.out.println(B.length);
         for (int i=0;i<L;i++){
             B[i]=T[i];
         }
         for(int x:B){
+            System.out.printf("%s ",x);
+        }
+        //Reversee copying array
+        System.out.println(" ");
+        int j=0;
+        for(j=L-1;j>=0;j--){
+            C[L-1-j]=T[j];
+        }
+        for(int x:C){
             System.out.printf("%s ",x);
         }
 
