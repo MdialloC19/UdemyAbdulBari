df79615841fd0be50a89c323238fb07b382ec317
MdialloC19 <mdiallo3211@gmail.com>
2023-04-27 23:58:27 +0000

    Pattern

---
+++ Pattern.java
@@ -0,0 +1,31 @@
+public class Pattern{
+
+    /*public static void main(String[] args) {
+            for(int i=1;i<5;i++)
+            {
+                for(int j=1;j<5;j++)
+                {
+                    if(i<=j)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.println("");
+            }
+    }*/
+    
+    public static void main(String[] args) {
+        for(int i=1;i<5;i++)
+            {
+                for(int j=1;j<5;j++)
+                {
+                    if(i+j>5)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                System.out.println("");
+            }
+    }
+    
+}
