f833e80fd995e832a0f17357d3111683c99bd7e0
MdialloC19 <mdiallo3211@gmail.com>
2023-05-23 15:11:11 +0000

    DynamicDispatch

---
+++ DynamicDispatch.java
@@ -0,0 +1,30 @@
+
+class Super {
+    public void meth1(){
+        System.out.println("Super Meth1");
+    }
+    public void meth2(){
+        System.out.println("Super Meth2");
+    }
+}
+class Sub extends Super {
+    @Override
+    public void meth2(){
+        System.out.println("Sub Meth2");
+    }
+    public void meth3(){
+            System.out.println("Sub Meth3");
+        }
+   
+}
+
+public class DynamicDispatch {
+    public static void main(String[] args) {
+
+        Super sup=new Sub();
+        sup.meth1();
+        sup.meth2();
+        // sup.meth3();
+        
+    }
+}
