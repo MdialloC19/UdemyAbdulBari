8dcfdfcf3e07d7301c7e373fa7c8abbb36d1af64
MdialloC19 <mdiallo3211@gmail.com>
2023-05-29 16:43:25 +0000

    Static inner class

--- localinner/LocalInner.java
+++ localinner/LocalInner.java
@@ -1,25 +1,39 @@
 package localinner;
 
-abstract class My{
-    abstract public void show ();
-}
+// abstract class My{
+//     abstract public void show ();
+// }
 
 class Outer{
-    public void display(){
-        
-            new My() {
-                public void show () {
-                    System.out.println(" Hello");
-                }
-            }.show();
+
+    int x=10;
+    static int y=20;
+    static class My{
+        public void show() {
+
+            // Outer i=new Outer();
+            // System.out.print(i.x);
+
+            System.out.println(y);
+            
+        }
     }
+
+    // public void display(){
+        
+    //         new My() {
+    //             public void show () {
+    //                 System.out.println(" Hello");
+    //             }
+    //         }.show();
+    // }
 }
 
 public class LocalInner {
     public static void main(String[] args) {
 
-        Outer O=new Outer();
-        O.display();
+        Outer.My O=new Outer.My();
+        O.show();
         
     }
     
