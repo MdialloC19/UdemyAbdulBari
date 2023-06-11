c7020946acdd79434fce250d89e3fc372ceb1c37
MdialloC19 <mdiallo3211@gmail.com>
2023-06-11 21:10:26 +0000

    final

---
+++ finaldemo/FinalDemo.java
@@ -0,0 +1,31 @@
+package finaldemo;
+
+class Test{
+
+    public final void show(){
+        System.out.println("Hello");
+    }
+
+}
+
+// class Test1 extends Test{
+
+//     public void show (){
+
+//     }
+// }
+
+public class FinalDemo {
+
+
+    
+    public static void main(String [] args){
+        final int X=10;
+        final float PI;
+
+        PI=3.14f;
+
+
+        System.out.println(X);
+    }
+}
