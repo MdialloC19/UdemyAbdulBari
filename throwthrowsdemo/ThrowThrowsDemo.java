5cc8e4fd33c575f2d80d1647c36da1e760fa8b42
MdialloC19 <mdiallo3211@gmail.com>
2023-06-27 18:54:02 +0000

    exception class

--- throwthrowsdemo/ThrowThrowsDemo.java
+++ throwthrowsdemo/ThrowThrowsDemo.java
@@ -1,21 +1,42 @@
 package throwthrowsdemo;
 
+import java.lang.*;
+
+class NegativeDimensionException extends Exception{
+    public String toString(){
+        return "Dimension of dimension should be negative";
+    }
+}
 public class ThrowThrowsDemo {
 
-    static int meth1(){
-        return 10/0;
+    static int area(int l, int b)throws NegativeDimensionException
+    {
+        if(l<0||b<0)
+            throw new NegativeDimensionException();
+        return l*b;
+    }
+    static void meth1()throws NegativeDimensionException
+    {
+        
+        System.out.println(area(-10, 5));
     }
 
-    static void meth2(){
-        meth1();
-    }
+    // static void meth2(){
+    //     meth1();
+    // }
 
-    static void meth3(){
-        meth2();
-    }
+    // static void meth3(){
+    //     meth2();
+    // }
 
-    public static void main(String[] args) {
-        meth3();
+    public static void main(String[] args)
+    {
+        try {
+            
+            meth1();
+        } catch (NegativeDimensionException e) {
+           System.out.println(e);
+        }
     }
     
 }
