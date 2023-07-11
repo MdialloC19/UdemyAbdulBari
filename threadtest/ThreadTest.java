5d8d17175669603f29bd94b7da64eac0351dff32
MdialloC19 <mdiallo3211@gmail.com>
2023-07-11 15:56:45 +0000

    Multitreading first method

---
+++ threadtest/ThreadTest.java
@@ -0,0 +1,26 @@
+package threadtest;
+
+class MyThread extends Thread {
+
+    public void run(){
+        int i=1;
+        while(true){
+            System.out.println(i+" Hello");
+            i++;
+        }
+    }
+}
+
+public class ThreadTest {
+    
+    public static void main (String [] args ){
+        MyThread t=new MyThread();
+        t.start();
+        int i=1;
+        while(true){
+            System.out.println(i+" World");
+            i++;
+        }
+        
+    }
+}
