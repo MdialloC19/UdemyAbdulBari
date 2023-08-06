3d7a8c53372f21d4699b80face0006fb7c91c8d8
MdialloC19 <mdiallo3211@gmail.com>
2023-08-06 14:38:40 +0000

    Lambda Method reference

--- lambda/LambdaDemo.java
+++ lambda/LambdaDemo.java
@@ -3,14 +3,14 @@ package lambda;
 
 interface MyLambda1{
 
-    public void display();
+    public int display(String str1,String str2);
    
 }
 
 class useLambda{
 
     public void callLambda(MyLambda1 ml){
-        ml.display();
+        ml.display(" "," ");
     }
 }
 
@@ -19,7 +19,7 @@ class Demo{
 
         useLambda ul=new useLambda();
 
-        ul.callLambda(()->{System.out.println("Hello World");});
+        // ul.callLambda((s,S1)->{System.out.println(s);});
 
         // MyLambda1 m1=()->{
         //     int count=0;
@@ -39,9 +39,23 @@ class Demo{
 // }
 public class LambdaDemo{
 
+    public LambdaDemo(String s){
+        System.out.println(s.toUpperCase());
+    }
+    public static void reverse(String str){
+        StringBuffer sb=new StringBuffer(str);
+        sb.reverse();
+        System.out.println(sb);
+    }
     public static void main(String[] args) {
-        Demo d=new Demo();
-        d.method1();
+
+        // MyLambda1 m1=LambdaDemo::new; for cosntructor
+        // Demo d=new Demo();
+        // d.method1();
+    //    MyLambda1 ml=System.out::println;
+    MyLambda1 m1=String::compareTo;
+
+       System.out.println(m1.display("Bonjour","Hello"));
     }
     
 }
